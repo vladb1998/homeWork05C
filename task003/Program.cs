1aba667f6cb0ea8a4c9b353b9f3b73d8352997a2
vlad <vladbelyvskij@gmail.com>
2022-08-20 23:57:09 +0300

    адайте массив вещественных чисел. айдите разницу между максимальным и минимальным элементов массива.

---
+++ task003/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = new int [10];
+int arraySize = array.Length;
+for (int i = 0; i < arraySize; i++)
+{
+    array[i] = new Random().Next(0,1000);
+}
+int min = array[0];
+int max = array[0];
+for (int i = 0; i < arraySize; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    else if (array[i]< min)
+    {
+        min = array[i];
+    }
+}
+
+Console.WriteLine("Array: [ " +  String.Join(" ; " , array) + " ] max = " + max + " min= " + min + " Разница между max и min  =  " + (max -  min) );
