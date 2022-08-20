21a66b2ca5a00c1f8f9d902d417508a45a7000d9
vlad <vladbelyvskij@gmail.com>
2022-08-20 23:54:21 +0300

    адайте массив заполненный случайными положительными трёхзначными числами. апишите программу, которая покажет количество чётных чисел в массиве.

---
+++ task001/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int [10];
+int arraySize = array.Length;
+int countEvenNumber = 0;
+for (int i = 0; i < arraySize; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    if (array[i]%2 == 0)
+    {
+        countEvenNumber++;
+    }
+}
+Console.WriteLine("Array: [ " +  String.Join(" ; " , array) + " ]  Количество четных чисел в массиве = " + countEvenNumber);
