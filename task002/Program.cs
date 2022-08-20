717a72623160f2c929b5cc7f919f4bbaa7ff7ed5
vlad <vladbelyvskij@gmail.com>
2022-08-20 23:56:18 +0300

    адайте одномерный массив, заполненный случайными числами. айдите сумму элементов, стоящих на нечётных позициях.

---
+++ task002/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int [10];
+int arraySize = array.Length;
+int unevenPositionSum = 0;
+for (int i = 0; i < arraySize; i++)
+{
+    array[i] = new Random().Next(0,1000);
+
+    if (i % 2 != 0)
+    {
+        unevenPositionSum += array[i];
+    }
+}
+
+Console.WriteLine("Array: [ " +  String.Join(" ; " , array) + " ]  Сумма элементов на нечётныч позициях = " + unevenPositionSum );
